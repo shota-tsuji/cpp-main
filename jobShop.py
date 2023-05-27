f58741867d40f6a931158abeca86f4b2bb6b9ee8
shota-tsuji <shotatuzi@gmail.com>
2023-05-27 09:31:37 +0900

    refactor: redefine all_steps as dict of list

--- jobShop.py
+++ jobShop.py
@@ -11,6 +11,12 @@ class RecipeStep:
         self.resource_id = resource_id
         self.order_number = order_number
 
+    def __str__(self):
+        return f'RecipeStep: {self.recipe_id}, {self.step_id}, {self.resource_id}, {self.order_number}'
+
+    def __repr__(self):
+        return f'RecipeStep: {self.recipe_id}, {self.step_id}, {self.resource_id}, {self.order_number}'
+
 class Resource:
 
     def __init__(self, resource_id, amount):
@@ -47,7 +53,6 @@ def main():
         [RecipeStep(1, 12, 1, 0, 1), RecipeStep(1, 13, 2, 1, 2)]
     ]
 
-    all_machines = range(len(resources))
     # Computes horizon dynamically as the sum of all durations.
     horizon = sum(step.duration for recipe in recipe_lists for step in recipe)
 
@@ -55,10 +60,8 @@ def main():
     model = cp_model.CpModel()
 
     # Named tuple to store information about created variables.
-    task_type = collections.namedtuple('task_type', 'start end interval')
+    task_type = collections.namedtuple('task_type', 'start end interval order step_id duration, resource_id')
     # Named tuple to manipulate solution information.
-    assigned_task_type = collections.namedtuple('assigned_task_type',
-                                                'start job index duration')
 
     # Creates job intervals and add to the corresponding machine lists.
     all_steps = {}
@@ -71,9 +74,12 @@ def main():
             end_var = model.NewIntVar(0, horizon, 'end' + suffix)
             interval_var = model.NewIntervalVar(start_var, step.duration, end_var,
                                                 'interval' + suffix)
-            all_steps[recipe_id, step_id] = task_type(start=start_var,
-                                                   end=end_var,
-                                                   interval=interval_var)
+            # use as dict
+            task = task_type(start=start_var, end=end_var, interval=interval_var, order=step.order_number, step_id=step.step_id, duration=step.duration, resource_id=step.resource_id)
+            if recipe_id in all_steps:
+                all_steps[recipe_id].append(task)
+            else:
+                all_steps[recipe_id] = [task]
             resource_intervals[step.resource_id].append(interval_var)
 
     # Disjunctive constraint 0: Resource capacity
@@ -86,22 +92,40 @@ def main():
             demands = [1] * len(intervals)
             model.AddCumulative(intervals, demands, resource.amount)
 
-    print(all_steps)
+    for recipe_id, steps in all_steps.items():
+        ordered_steps = sorted(steps, key=lambda step: step.order)
+        print(ordered_steps)
 
     # Precedences inside a job.
     for job_id, job in enumerate(recipes_data):
         for task_id in range(len(job) - 1):
-            model.Add(all_steps[job_id, task_id +
-                                1].start >= all_steps[job_id, task_id].end)
+            # use as array
+            model.Add(all_steps[job_id][task_id + 1].start >= all_steps[job_id][task_id].end)
+
+
 
     # Makespan objective.
     obj_var = model.NewIntVar(0, horizon, 'makespan')
-    model.AddMaxEquality(obj_var, [
-        # fix on d0 as recipe_id ex. (0, ?) -> (start, end, interval)
-        # レシピごとに一番最後の工程の tuple の end を見ている
-        all_steps[recipe_id, len(recipe) - 1].end
-        for recipe_id, recipe in enumerate(recipe_lists)
-    ])
+    # fix on d0 as recipe_id ex. (0, ?) -> (start, end, interval)
+    # レシピごとに一番最後の工程の tuple の end を見ている
+    recipe_ends = []
+    for recipe_id, steps in all_steps.items():
+        last_step = sorted(steps, key=lambda step: step.order)[-1]
+        print(last_step)
+        recipe_ends.append(last_step.end)
+    #for recipe in recipe_lists:
+    #    last_step = sorted(recipe, key=lambda step: step.order_number)[-1]
+    #    print(last_step)
+    #    #all_steps[recipe]
+    #    print(f'{last_step.recipe_id}, {last_step.step_id}')
+    #    end = all_steps[last_step.recipe_id][last_step.step_id].end
+    #    print(end)
+    #    recipe_ends.append(end)
+    # use as array
+    #v = [all_steps[recipe_id, len(recipe) - 1].end for recipe_id, recipe in enumerate(recipe_lists)]
+    #print(v)
+
+    model.AddMaxEquality(obj_var, recipe_ends)
     model.Minimize(obj_var)
 
     # Creates the solver and solve.
@@ -111,6 +135,7 @@ def main():
     if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
         print('Solution:')
         # recipe_lists と variable.start の組み合わせがとりたい
+        # use as dict
         step_outputs = get_step_outputs(solver, all_steps, recipe_lists)
         #print(list(map(lambda s: s.to_string(), step_outputs)))
         for step_output in step_outputs:
@@ -128,10 +153,10 @@ def main():
 
 def get_step_outputs(solver, all_steps, recipe_lists):
     step_outputs = []
-    for recipe_id, recipe in enumerate(recipe_lists):
-        for step_id, step in enumerate(recipe):
-            start_time = solver.Value(all_steps[recipe_id, step_id].start)
-            step_outputs.append(StepOutput(step.recipe_id, step.step_id, step.duration, step.resource_id, start_time))
+    for recipe_id, steps in all_steps.items():
+        for step in steps:
+            start_time = solver.Value(step.start)
+            step_outputs.append(StepOutput(recipe_id, step.step_id, step.duration, step.resource_id, start_time))
 
     return step_outputs
 
